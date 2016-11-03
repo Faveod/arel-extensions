a39ed7b5bf76ab341387c87c905171e542d70bbb
Yann Azoury <yann.azoury@faveod.com>
2016-11-03 15:06:30 +0100

    Sql Server

--- arel_extensions.gemspec
+++ arel_extensions.gemspec
@@ -1,20 +1,21 @@
 # # -*- encoding: utf-8 -*-
 $:.push File.expand_path("../lib", __FILE__)
 # require "arel_extensions"
+require "arel_extensions/version"
 
 Gem::Specification.new do |s|
   s.name        = "arel_extensions"
-  s.version     = '0.8.8'
+  s.version     = ArelExtensions::VERSION
   s.platform    = Gem::Platform::RUBY
   s.authors     = ["Yann Azoury", "Mathilde Pechdimaldjian", "Félix Bellanger"]
   s.email       = ["yann.azoury@faveod.com", "mathilde.pechdimaldjian@gmail.com", "felix.bellanger@faveod.com"]
   s.homepage    = "https://github.com/Faveod/arel-extensions"
   s.description = "Adds new features to Arel"
   s.summary     = "Extending Arel"
-  s.license     = %q{MIT}
+  s.license     = 'MIT'
 
   s.rdoc_options = ["--main", "README.md"]
-  s.extra_rdoc_files = ["MIT-LICENSE.txt", "README.md"]
+  s.extra_rdoc_files = ["MIT-LICENSE.txt", "README.md", 'functions.html']
 
   # Manifest
   s.files         = `git ls-files`.split("\n")
--- lib/arel_extensions/visitors.rb
+++ lib/arel_extensions/visitors.rb
@@ -9,7 +9,7 @@ Arel::Visitors::MSSQL.class_eval do
 	include ArelExtensions::Visitors::MSSQL
 end
 
-# puts "VISITORS: " + Arel::Visitors::VISITORS.inspect
+puts "[SQL_SERVER_LOADING_DEBUG] VISITORS: " + Arel::Visitors::VISITORS.inspect
 
 if Arel::Visitors::VISITORS['sqlserver'] && Arel::Visitors::VISITORS['sqlserver'] != Arel::Visitors::MSSQL
 	Arel::Visitors::VISITORS['sqlserver'].class_eval do
@@ -17,4 +17,16 @@ if Arel::Visitors::VISITORS['sqlserver'] && Arel::Visitors::VISITORS['sqlserver'
 	end 
 end
 
-# puts "SQLServer constants: #{Arel::Visitors.constants.inspect}"
+puts "[SQL_SERVER_LOADING_DEBUG] Arel::Visitors constants: #{Arel::Visitors.constants.inspect}"
+puts "[SQL_SERVER_LOADING_DEBUG] Arel::Visitors::ENGINE_VISITORS constants: #{Arel::Visitors::ENGINE_VISITORS.inspect}"
+
+puts "ActiveRecord::ConnectionAdapters::SQLServerAdapter loaded #{ActiveRecord::ConnectionAdapters::SQLServerAdapter}" rescue "no ActiveRecord::ConnectionAdapters::SQLServerAdapter"
+puts("[SQL_SERVER_LOADING_DEBUG] Arel::Visitors::SQLServer constants: #{Arel::Visitors::SQLServer.inspect}") rescue puts "no Arel::Visitors::SQLServer"
+
+begin 
+# require('activerecord-sqlserver-adapter/arel/visitors/sqlserver')
+puts "ActiveRecord::ConnectionAdapters::SQLServerAdapter loaded #{ActiveRecord::ConnectionAdapters::SQLServerAdapter}" rescue "no ActiveRecord::ConnectionAdapters::SQLServerAdapter"
+puts("[SQL_SERVER_LOADING_DEBUG] Arel::Visitors::SQLServer constants: #{Arel::Visitors::SQLServer.inspect}") rescue puts "no Arel::Visitors::SQLServer"
+rescue => e
+ 	"can't load activerecord-sqlserver-adapter/arel/visitors/sqlserver #{e.inspect}"
+ end
