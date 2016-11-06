b261103901337d12e48c4716878859515ab454af
Yann Azoury <yann.azoury@faveod.com>
2016-11-06 20:56:27 +0100

    Sql Server

--- lib/arel_extensions/visitors/mssql.rb
+++ lib/arel_extensions/visitors/mssql.rb
@@ -155,9 +155,9 @@ module ArelExtensions
       end
 
       def visit_ArelExtensions_Nodes_Blank o, collector
-        collector << '(LEN(LTRIM(RTRIM(ISNULL('
+        collector << 'CASE WHEN LEN(LTRIM(RTRIM(ISNULL('
         collector = visit o.left, collector
-        collector << ", '')))) = 0)"
+        collector << ", '')))) = 0 THEN 1 ELSE 0 END"
         collector
       end
 
