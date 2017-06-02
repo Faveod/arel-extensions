14afbecb9993602bb7254ea98cc1ba2b11a8f829
fanaho23 <pierreche.thill@gmail.com>
2017-06-02 14:19:28 +0200

    Modification du fichier download.sh, modifié
    Conditions des if initiales

--- .travis/oracle/download.sh
+++ .travis/oracle/download.sh
@@ -1,17 +1,17 @@
 #!/bin/sh -e
 # vim: set et sw=2 ts=2:
-if [ -z "$ORACLE_COOKIE" ]; then  # -z variable vide. on veut savoir si la variable $ORACLE_COOKIE (contient:ORACLE_COOKIE=sqldev) est vide, si elle l'est echo...
+if [ -n "$ORACLE_COOKIE" ]; then  # -z variable vide. on veut savoir si la variable $ORACLE_COOKIE (contient:ORACLE_COOKIE=sqldev) est vide, si elle l'est echo...
 	echo "Missing ORACLE_COOKIE environment variable!"
 	 exit 1
 fi 
-if [ -z "$ORACLE_FILE" ]; then # -z variable vide. on veut savoir si la variable $ORACLE_FILE (contient:ORACLE_FILE=oracle11g/xe/oracle-xe-11.2.0-1.0.x86_64.rpm.zip) est vide, si elle l'est echo...
+if [ -n "$ORACLE_FILE" ]; then # -z variable vide. on veut savoir si la variable $ORACLE_FILE (contient:ORACLE_FILE=oracle11g/xe/oracle-xe-11.2.0-1.0.x86_64.rpm.zip) est vide, si elle l'est echo...
 	echo "Missing ORACLE_FILE environment variable!"
 	 exit 1 
 fi
 
 ORACLE_DOWNLOAD_FILE="$(basename "$ORACLE_FILE")" #basename permet de récuperer l'enssemble du chemin sauf "le dernier élément" ORACLE_FILE=oracle11g/xe"/oracle-xe-11.2.0-1.0.x86_64.rpm.zip"
 
-if [ -z "$ORACLE_DOWNLOAD_DIR" ];#verifie si la variable d'environemment est vide
+if [ -n "$ORACLE_DOWNLOAD_DIR" ];#verifie si la variable d'environemment est vide
  then # defini le début du bloque d'instruction
 	mkdir -p "$ORACLE_DOWNLOAD_DIR" #Si les répertoires parent n'existent pas, cette commande les crée.
 	ORACLE_DOWNLOAD_FILE="$(readlink -f "$ORACLE_DOWNLOAD_DIR")/$ORACLE_DOWNLOAD_FILE"
