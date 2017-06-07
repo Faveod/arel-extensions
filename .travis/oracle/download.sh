959426cc09b692ae33d39bc41377a830d53b5421
fanaho23 <pierreche.thill@gmail.com>
2017-06-07 11:24:21 +0200

    Modification de .travis.yml et download.sh
    Retour a début (download.sh)
    Modification de la mise en cache pour oracle (.travis.yml)

--- .travis/oracle/download.sh
+++ .travis/oracle/download.sh
@@ -1,10 +1,7 @@
 #!/bin/sh -e
 # vim: set et sw=2 ts=2:
 
-if [ -n "$ORACLE_COOKIE" ]; then 
- echo "Missing ORACLE_COOKIE environment variable!"
- exit 1
-fi
+[ -n "$ORACLE_COOKIE" ] || { echo "Missing ORACLE_COOKIE environment variable!"; exit 1; }
 [ -n "$ORACLE_FILE" ] || { echo "Missing ORACLE_FILE environment variable!"; exit 1; }
 
 ORACLE_DOWNLOAD_FILE="$(basename "$ORACLE_FILE")"
