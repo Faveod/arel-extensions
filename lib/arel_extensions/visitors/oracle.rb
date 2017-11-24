ff5afb4e20acfff60cf1a5079332c0e6e6ab32a8
jdelporte <julien.delporte@gmail.com>
2017-11-24 17:51:15 +0100

    agnostic tests for union

--- lib/arel_extensions/visitors/oracle.rb
+++ lib/arel_extensions/visitors/oracle.rb
@@ -327,6 +327,14 @@ module ArelExtensions
       end
     end
 
+	
+		def visit_ArelExtensions_Nodes_Union o, collector
+			collector = visit o.left, collector
+			collector << " UNION "
+			collector = visit o.right, collector
+			collector
+		end
+
     end
   end
 end
