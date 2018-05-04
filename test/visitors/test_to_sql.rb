67ad1f063e4403af852d3e1dcd90158740cbb847
jdelporte <julien.delporte@faveod.com>
2018-05-04 19:37:51 +0200

    formatting number in postgresql is working

--- test/visitors/test_to_sql.rb
+++ test/visitors/test_to_sql.rb
@@ -303,12 +303,7 @@ module ArelExtensions
 	  end
 	  	  
 	  
-	  it "should be possible to specify a cool format on number" do	  
-		#puts @price.format_number("$$ %+030.2e €€","fr_FR").to_sql
-		compile(@price.format_number("$$ %+030.2e €€","fr_FR"))
-			.must_be_like %{CONCAT('$$ ', CASE \"products\".\"price\" WHEN 0 THEN 0 ELSE CONCAT('', CASE WHEN \"products\".\"price\" < 0 THEN '-' ELSE '+' END, CASE WHEN (ABS(30) - (LENGTH(CONCAT(FORMAT(ABS(\"products\".\"price\") / POW(10, FLOOR(LOG10(ABS(\"products\".\"price\")))), 2, 'fr_FR'), 'e', FORMAT(FLOOR(LOG10(ABS(\"products\".\"price\"))), 0))) + LENGTH(CASE WHEN \"products\".\"price\" < 0 THEN '-' ELSE '+' END))) > 0 THEN REPEAT('0', (ABS(30) - (LENGTH(CONCAT(FORMAT(ABS(\"products\".\"price\") / POW(10, FLOOR(LOG10(ABS(\"products\".\"price\")))), 2, 'fr_FR'), 'e', FORMAT(FLOOR(LOG10(ABS(\"products\".\"price\"))), 0))) + LENGTH(CASE WHEN \"products\".\"price\" < 0 THEN '-' ELSE '+' END)))) ELSE '' END, CONCAT(FORMAT(ABS(\"products\".\"price\") / POW(10, FLOOR(LOG10(ABS(\"products\".\"price\")))), 2, 'fr_FR'), 'e', FORMAT(FLOOR(LOG10(ABS(\"products\".\"price\"))), 0)), '') END, ' €€')}
-	  end
-	  
+	 	  
 	  puts "AREL VERSION : " + Arel::VERSION.to_s
     end
   end
