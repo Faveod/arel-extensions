e7e54a05aa3ac989f5057d8981f1119165e233ac
Akim Demaille <akim.demaille@gmail.com>
2020-04-23 15:32:25 +0200

    not_in: also support mixtures of range and values and nils
    Just like `in`.

--- lib/arel_extensions/predications.rb
+++ lib/arel_extensions/predications.rb
@@ -25,6 +25,10 @@ module ArelExtensions
       case other
       when Range
         self.between(other)
+      when nil
+        self.is_null
+      when Arel::SelectManager
+        Arel::Nodes::In.new(self, other.ast)
       when Enumerable
         nils, values   = other.partition{ |v| v.nil? }
         ranges, values = values.partition{ |v| v.is_a?(Range) || v.is_a?(Arel::SelectManager)}
@@ -38,37 +42,33 @@ module ArelExtensions
               else [Arel::Nodes::In.new(self, quoted_array(values))] end) \
           + ranges.uniq.map { |r| self.in(r) }
         Arel::Nodes::Or.new clauses
-      when nil
-        self.is_null
-      when Arel::SelectManager
-        Arel::Nodes::In.new(self, other.ast)
       else
         Arel::Nodes::In.new(self, quoted_node(other))
       end
     end
 
-    def not_in(other) #In should handle nil element in the Array
+    def not_in(*other) #In should handle nil element in the Array
+      other = other.first if other.size == 0 || other.size == 1
       case other
-      when Range
-        Arel::Nodes::Not.new(self.between(other))
-      when Enumerable
-        if other.include?(nil)
-          other.delete(nil)
-          case other.length
-          when 0
-            self.is_not_null
-          when 1
-            self.is_not_null.and(self!=other[0])
-          else
-            self.is_not_null.and(Arel::Nodes::NotIn.new(self,quoted_array(other)))
-          end
-        else
-          Arel::Nodes::NotIn.new(self,quoted_array(other))
-        end
       when nil
         self.is_not_null
+      when Range
+        Arel::Nodes::Not.new(self.between(other))
       when Arel::SelectManager
         Arel::Nodes::NotIn.new(self, other.ast)
+      when Enumerable
+        nils, values   = other.partition{ |v| v.nil? }
+        ranges, values = values.partition{ |v| v.is_a?(Range) || v.is_a?(Arel::SelectManager)}
+
+        # In order of (imagined) decreasing efficiency: nil, values, and then more complex.
+        clauses =
+          nils.uniq.map { |r| self.not_in(r) } \
+          + (case values.uniq.size
+              when 0 then []
+              when 1 then [self != values[0]]
+              else [Arel::Nodes::NotIn.new(self, quoted_array(values))] end) \
+          + ranges.uniq.map { |r| self.not_in(r) }
+        Arel::Nodes::And.new clauses
       else
         Arel::Nodes::NotIn.new(self,quoted_node(other))
       end
