b3162650a1633d5b0fbdbb28003199422aa51e17
Julien Delporte <julien.delporte@faveod.com>
2020-12-30 13:51:25 +0100

    github ci: WIP

--- generate_gems.sh
+++ generate_gems.sh
@@ -8,7 +8,7 @@ gem build ./arel_extensions.gemspec
 # VERSION ~> 2
 cp ./version_v2.rb lib/arel_extensions/version.rb
 mv ./arel_extensions.gemspec ./arel_extensions.gemspec.bck
-cp ./gemspec_v2/arel_extensions-v2.gemspec ./arel_extensions.gemspec
+cp ./gemspecs/arel_extensions-v2.gemspec ./arel_extensions.gemspec
 gem build ./arel_extensions.gemspec
 cp ./version_v1.rb lib/arel_extensions/version.rb
-cp ./arel_extensions.gemspec.bck ./arel_extensions.gemspec
+cp ./gemspecs/arel_extensions-v1.gemspec ./arel_extensions.gemspec
