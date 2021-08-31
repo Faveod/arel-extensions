f5de86eca6cb5772806dc575fd76b55b43066a6c
Akim Demaille <akim.demaille@gmail.com>
2021-08-31 10:05:30 +0200

    style: formatting changes

--- lib/arel_extensions/insert_manager.rb
+++ lib/arel_extensions/insert_manager.rb
@@ -3,23 +3,23 @@ require 'arel'
 module ArelExtensions
   module InsertManager
     def bulk_insert(cols, data)
-    res_columns = []
-    case cols.first
-    when String, Symbol
-      cols.each { |c|
-        res_columns << @ast.relation[c]
-      }
-    when Array
-      if String === cols.first.first
-        res_columns = cols.map {|c| [@ast.relation[c.first]] }
-      elsif Arel::Attributes::Attribute == cols.first.first
-        res_columns = cols
+      res_columns = []
+      case cols.first
+      when String, Symbol
+        cols.each { |c|
+          res_columns << @ast.relation[c]
+        }
+      when Array
+        if String === cols.first.first
+          res_columns = cols.map {|c| [@ast.relation[c.first]] }
+        elsif Arel::Attributes::Attribute == cols.first.first
+          res_columns = cols
+        end
+      when NilClass
+        res_columns = @ast.relation.columns
       end
-    when NilClass
-      res_columns = @ast.relation.columns
-    end
-    self.values = BulkValues.new(res_columns, data)
-    @ast.columns = res_columns
+      self.values = BulkValues.new(res_columns, data)
+      @ast.columns = res_columns
     end
 
     class BulkValues < Arel::Nodes::Node
