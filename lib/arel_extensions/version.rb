10e6f418c167f956817d68d9748c8da86c222dac
Julien Delporte <julien.delporte@faveod.com>
2021-12-01 11:59:17 +0100

    v1.2.26/v.2.0.23

--- lib/arel_extensions/version.rb
+++ lib/arel_extensions/version.rb
@@ -1,3 +1,3 @@
 module ArelExtensions
-  VERSION = "1.2.25".freeze
+  VERSION = "1.2.26".freeze
 end
--- version_v2.rb
+++ version_v2.rb
@@ -1,3 +1,3 @@
 module ArelExtensions
-  VERSION = "2.0.22".freeze
+  VERSION = "2.0.23".freeze
 end
