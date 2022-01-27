4d17c95053da3c8a05a4c9c0fc819165628c1942
Anthony Truskinger <anthony.truskinger@gmail.com>
2022-01-27 19:06:39 +1000

    Fix double quotation issue for aliases in groups

--- lib/arel_extensions/visitors/postgresql.rb
+++ lib/arel_extensions/visitors/postgresql.rb
@@ -86,9 +86,15 @@ module ArelExtensions
         else
           collector = visit o.left, collector
         end
-        collector << " AS \""
+        collector << " AS "
+
+        # sometimes these values are already quoted, if they are, don't double quote it
+        quote = o.right.is_a?(Arel::Nodes::SqlLiteral) && o.right[0] != '"' && o.right[-1] != '"'
+        
+        collector << '"' if quote
         collector = visit o.right, collector
-        collector << "\""
+        collector << '"' if quote
+        
         collector
       end
 
