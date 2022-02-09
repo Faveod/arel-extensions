7bb38a86aec99aff26d317b03aa9c7bc904a9e82
Firas al-Khalil <firas.alkhalil@faveod.com>
2022-02-09 10:32:06 +0100

    test: load (require) proper db library for tests
    1. If the db library is not loaded then tests are flaky
    2. If all db libraries are loaded then tests are flaky
    
    For some reason Arel will have undefined behavior when no db gem or all
    db gems are loaded.

--- test/arelx_test_helper.rb
+++ test/arelx_test_helper.rb
@@ -6,6 +6,52 @@ require 'active_record'
 
 require 'support/fake_record'
 
+def colored(color, msg)
+  ENV["TERM"] =~ /^xterm|-256color$/ ? "\x1b[#{color}m#{msg}\x1b[89m\x1b[0m"  : "#{msg}"
+end
+
+YELLOW = "33"
+
+def warn(msg)
+  $stderr.puts(colored(YELLOW, msg))
+end
+
+# Load gems specific to databases
+# NOTE: It's strongly advised to test each database on its own.
+#       Loading multiple backend gems leads to undefined behavior according to
+#       tests; the backend might not recognize the correct DB visitor and will
+#       fallback to `ToSQL` and screw all tests.
+db_and_gem =  if RUBY_ENGINE == 'jruby'
+                {
+                  'mysql'      => 'activerecord-jdbcmysql-adapter',
+                  'postgresql' => 'activerecord-jdbcpostgresql-adapter',
+                  'sqlite'     => 'activerecord-jdbcsqlite3-adapter',
+                  'ibm_db'     => 'ibm_db',
+                  'oracle'     => 'activerecord-oracle_enhanced-adapter',
+                  'mssql'      => 'activerecord-jdbcsqlserver-adapter'
+                }
+              else
+                {
+                  'mysql'      => 'mysql2',
+                  'postgresql' => 'pg',
+                  'sqlite'     => 'sqlite3',
+                  'ibm_db'     => 'ibm_db',
+                  'oracle'     => 'activerecord-oracle_enhanced-adapter',
+                  'mssql'      => 'activerecord-sqlserver-adapter'
+                }
+              end
+
+def load_lib(gem)
+  begin
+    Gem::Specification.find_by_name(gem)
+    require gem
+  rescue Gem::MissingSpecError
+    warn "Warning: failed to load gem #{gem}. Are you sure it's installed?"
+  end
+end
+
+load_lib(db_and_gem[ENV['DB']]) if ENV['DB']&.strip&.empty?
+
 require 'arel_extensions'
 Arel::Table.engine = FakeRecord::Base.new
 
