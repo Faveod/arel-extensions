Faveod/arel-extensions

commit 1515092c5e036b7ebaaefd137ba4f60fe26b519c

format: datetime: add support for {src => dst} hash for timezones
This will allow us to explicitly set the original datetime's timezone
before conversion to the desired one.

lib/arel_extensions/visitors/oracle.rb
@@ -449,9 +449,16 @@ module ArelExtensions
         collector << "TO_CHAR("
         collector << "CAST(" if o.time_zone
         collector = visit o.left, collector
-        if o.time_zone
-          collector << " as timestamp) at time zone "
-          collector = visit o.time_zone, collector
+        case o.time_zone
+        when Hash
+          src_tz, dst_tz = o.time_zone.first
+          collector << ' as timestamp) at time zone '
+          collector = visit Arel::Nodes.build_quoted(src_tz), collector
+          collecto < ' at time zone '
+          collector = visit Arel::Nodes.build_quoted(dst_tz), collector
+        when String
+          collector << ' as timestamp) at time zone '
+          collector = visit Arel::Nodes.build_quoted(o.time_zone), collector
         end
         collector << COMMA
         collector = visit Arel::Nodes.build_quoted(fmt), collector

lib/arel_extensions/nodes/format.rb
@@ -10,7 +10,7 @@ module ArelExtensions
       def initialize expr
         col = expr[0]
         @iso_format = convert_format(expr[1])
-        @time_zone  = Arel::Nodes.build_quoted(expr[2]) if expr[2]
+        @time_zone  = expr[2]
         @col_type = type_of_attribute(col)
         super [col, convert_to_string_node(@iso_format)]
       end

lib/arel_extensions/visitors/mssql.rb
@@ -276,9 +276,16 @@ module ArelExtensions
             collector << ' '
           end
           collector = visit o.left, collector
-          if o.time_zone
-            collector << ") AT TIME ZONE 'UTC' AT TIME ZONE "
-            collector = visit o.time_zone, collector
+          case o.time_zone
+          when Hash
+            src_tz, dst_tz = o.time_zone.first
+            collector << ') AT TIME ZONE '
+            collector = visit Arel::Nodes.build_quoted(src_tz), collector
+            collector << ' AT TIME ZONE '
+            collector = visit Arel::Nodes.build_quoted(dst_tz), collector
+          when String
+            collector << ') AT TIME ZONE '
+            collector = visit Arel::Nodes.build_quoted(o.time_zone), collector
           end
           collector << LOADED_VISITOR::COMMA
           collector << fmt.to_s
@@ -307,9 +314,16 @@ module ArelExtensions
                 collector << ' '
               end
               collector = visit o.left, collector
-              if o.time_zone
-                collector << ") AT TIME ZONE 'UTC' AT TIME ZONE "
-                collector = visit o.time_zone, collector
+              case o.time_zone
+              when Hash
+                src_tz, dst_tz = o.time_zone.first.first, o.time_zone.first.second
+                collector << ") AT TIME ZONE "
+                collector = visit Arel::Nodes.build_quoted(src_tz), collector
+                collector << " AT TIME ZONE "
+                collector = visit Arel::Nodes.build_quoted(dst_tz), collector
+              when String
+                collector << ") AT TIME ZONE "
+                collector = visit Arel::Nodes.build_quoted(o.time_zone), collector
               end
               collector << ')'
               collector << ')'                                  if !fmt

lib/arel_extensions/visitors/mysql.rb
@@ -211,9 +211,17 @@ module ArelExtensions
           collector << "DATE_FORMAT("
           collector << "CONVERT_TZ(" if o.time_zone
           collector = visit o.left, collector
-          if o.time_zone
+          case o.time_zone
+          when Hash
+            src_tz, dst_tz = o.time_zone.first
+            collector << COMMA
+            collector = visit Arel::Nodes.build_quoted(src_tz), collector
+            collector << COMMA
+            collector = visit Arel::Nodes.build_quoted(dst_tz), collector
+            collector << ')'
+          when String
             collector << COMMA << "'UTC'" << COMMA
-            collector = visit o.time_zone, collector
+            collector = visit Arel::Nodes.build_quoted(o.time_zone), collector
             collector << ')'
           end
           collector << COMMA

lib/arel_extensions/visitors/postgresql.rb
@@ -175,10 +175,18 @@ module ArelExtensions
       def visit_ArelExtensions_Nodes_Format o, collector
         fmt = ArelExtensions::Visitors::strftime_to_format(o.iso_format, DATE_FORMAT_DIRECTIVES)
         collector << "TO_CHAR("
+        collector << '(' if o.time_zone
         collector = visit o.left, collector
-        if o.time_zone
-          collector << " AT TIME ZONE "
-          collector = visit o.time_zone, collector
+        case o.time_zone
+        when Hash
+          src_tz, dst_tz = o.time_zone.first
+          collector << ') AT TIME ZONE '
+          collector = visit Arel::Nodes.build_quoted(src_tz), collector
+          collector << ' AT TIME ZONE '
+          collector = visit Arel::Nodes.build_quoted(dst_tz), collector
+        when String
+          collector << ') AT TIME ZONE '
+          collector = visit Arel::Nodes.build_quoted(o.time_zone), collector
         end
         collector << COMMA
         collector = visit Arel::Nodes.build_quoted(fmt), collector

test/with_ar/all_agnostic_test.rb
@@ -403,35 +403,32 @@ module ArelExtensions
                             'tahiti'    => 'Hawaiian Standard Time',
                             'paris'     => 'Central European Standard Time'
                            },
-          'mysql'       => {
+          'posix'       => {
                             'utc'       => 'UTC',
                             'sao_paulo' => 'America/Sao_Paulo',
                             'tahiti'    => 'Pacific/Tahiti',
                             'paris'     => 'Europe/Paris'
-                           },
-          'oracle'  => {
-                            'utc'       => 'UTC',
-                            'sao_paulo' => 'America/Sao_Paulo',
-                            'tahiti'    => 'Pacific/Tahiti',
-                            'paris'     => 'Europe/Paris'
-                            },
-          'postgresql'  => {
-                            'utc'       => 'UTC',
-                            'sao_paulo' => 'America/Sao Paulo (-03)',
-                            'tahiti'    => 'Pacific/Tahiti (-10)',
-                            'paris'     => 'Europe/Paris'
-                            },
+                           }
         }
 
-        tz = time_zones[ENV['DB']]
-        skip "Unsupported timezone conversion for DB=#{ENV['DB']}" if tz.nil?
+        skip "Unsupported timezone conversion for DB=#{ENV['DB']}" if !['mssql', 'mysql', 'oracle', 'postgresql'].include?(ENV['DB'])
+
+        tz = ENV['DB'] == 'mssql' ? time_zones['mssql'] : time_zones['posix']
+
         assert_equal '2014/03/03 12:42:00', t(@lucas, @updated_at.format('%Y/%m/%d %H:%M:%S', tz['utc']))
-        assert_equal '2014/03/03 09:42:00', t(@lucas, @updated_at.format('%Y/%m/%d %H:%M:%S', tz['sao_paulo']))
-        assert_equal '2014/03/03 02:42:00', t(@lucas, @updated_at.format('%Y/%m/%d %H:%M:%S', tz['tahiti']))
+        assert_equal '2014/03/03 09:42:00', t(@lucas, @updated_at.format('%Y/%m/%d %H:%M:%S', { tz['utc'] => tz['sao_paulo'] }))
+        assert_equal '2014/03/03 02:42:00', t(@lucas, @updated_at.format('%Y/%m/%d %H:%M:%S', { tz['utc'] => tz['tahiti'] }))
+        assert_equal '2014/03/03 13:42:00', t(@lucas, @updated_at.format('%Y/%m/%d %H:%M:%S', { tz['utc'] => tz['paris'] }))
 
         # Winter/Summer time
-        assert_equal '2022/02/01 11:42:00', t(@lucas, Arel::Nodes.build_quoted('2022-02-01 10:42:00').cast(:datetime).format('%Y/%m/%d %H:%M:%S', tz['paris']))
-        assert_equal '2022/08/01 12:42:00', t(@lucas, Arel::Nodes.build_quoted('2022-08-01 10:42:00').cast(:datetime).format('%Y/%m/%d %H:%M:%S', tz['paris']))
+        assert_equal '2014/08/03 14:42:00', t(@lucas, (@updated_at + 5.months).format('%Y/%m/%d %H:%M:%S', { tz['utc'] => tz['paris'] }))
+        if ENV['DB'] == 'mssql'
+          assert_equal '2022/02/01 11:42:00', t(@lucas, Arel::Nodes.build_quoted('2022-02-01 10:42:00').cast(:datetime).format('%Y/%m/%d %H:%M:%S', { tz['utc'] => tz['paris'] }))
+          assert_equal '2022/08/01 12:42:00', t(@lucas, Arel::Nodes.build_quoted('2022-08-01 10:42:00').cast(:datetime).format('%Y/%m/%d %H:%M:%S', { tz['utc'] => tz['paris'] }))
+        else
+          assert_equal '2022/02/01 11:42:00', t(@lucas, Arel::Nodes.build_quoted('2022-02-01 10:42:00').cast(:datetime).format('%Y/%m/%d %H:%M:%S', tz['paris']))
+          assert_equal '2022/08/01 12:42:00', t(@lucas, Arel::Nodes.build_quoted('2022-08-01 10:42:00').cast(:datetime).format('%Y/%m/%d %H:%M:%S', tz['paris']))
+        end
       end
 
       def test_coalesce

commit 7ba15bfde5fb23728d9e3ebb6d35b39a6d79f1f8

oracle: fix typo
Introduced in 1515092c5e036b7ebaaefd137ba4f60fe26b519c.

lib/arel_extensions/visitors/oracle.rb
@@ -455,7 +455,7 @@ module ArelExtensions
           src_tz, dst_tz = o.time_zone.first
           collector << ' as timestamp) at time zone '
           collector = visit Arel.quoted(src_tz), collector
-          collecto < ' at time zone '
+          collector < ' at time zone '
           collector = visit Arel.quoted(dst_tz), collector
         when String
           collector << ' as timestamp) at time zone '