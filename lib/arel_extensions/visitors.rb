5c038e41260e2e573ff08736bc299960e2e3d1c9
Firas al-Khalil <firas.alkhalil@faveod.com>
2024-07-29 10:05:06 +0200

    mssql: refactor

--- lib/arel_extensions/visitors.rb
+++ lib/arel_extensions/visitors.rb
@@ -15,16 +15,13 @@ end
 require 'arel_extensions/visitors/convert_format'
 require 'arel_extensions/visitors/to_sql'
 require 'arel_extensions/visitors/mysql'
+require 'arel_extensions/visitors/mssql'
 require 'arel_extensions/visitors/postgresql'
 require 'arel_extensions/visitors/sqlite'
 
 if defined?(Arel::Visitors::Oracle)
   require 'arel_extensions/visitors/oracle'
   require 'arel_extensions/visitors/oracle12'
-end
-
-if defined?(Arel::Visitors::SQLServer) || defined?(Arel::Visitors::MSSQL)
-  require 'arel_extensions/visitors/mssql'
 end
 
 if defined?(Arel::Visitors::SQLServer)
@@ -54,22 +51,7 @@ if defined?(Arel::Visitors::MSSQL)
   class Arel::Visitors::MSSQL
     include ArelExtensions::Visitors::MSSQL
 
-    alias_method(:old_visit_Arel_Nodes_As, :visit_Arel_Nodes_As) rescue nil
-    def visit_Arel_Nodes_As o, collector
-      if o.left.is_a?(Arel::Nodes::Binary)
-        collector << '('
-        collector = visit o.left, collector
-        collector << ')'
-      else
-        collector = visit o.left, collector
-      end
-      collector << ' AS ['
-      collector = visit o.right, collector
-      collector << ']'
-      collector
-    end
-
-    alias_method(:old_visit_Arel_Nodes_SelectStatement, :visit_Arel_Nodes_SelectStatement) rescue nil
+    alias_method(:old_visit_Arel_Nodes_SelectStatement, :visit_Arel_Nodes_SelectStatement)
     def visit_Arel_Nodes_SelectStatement o, collector
       if !collector.value.blank? && o.limit.blank? && o.offset.blank?
         o = o.dup
@@ -81,47 +63,38 @@ if defined?(Arel::Visitors::MSSQL)
 end
 
 if defined?(Arel::Visitors::SQLServer)
-  if Arel::VERSION.to_i >= 6
-    class Arel::Visitors::SQLServer
-      include ArelExtensions::Visitors::MSSQL
+  class Arel::Visitors::SQLServer
+    include ArelExtensions::Visitors::MSSQL
 
-      alias_method(:old_visit_Arel_Nodes_SelectStatement, :visit_Arel_Nodes_SelectStatement) rescue nil
-      def visit_Arel_Nodes_SelectStatement o, collector
-        if !collector.value.blank? && o.limit.blank? && o.offset.blank?
-          o = o.dup
-          o.orders = []
-        end
-        old_visit_Arel_Nodes_SelectStatement(o, collector)
+    # There's a bug when working with jruby 9.4 that prevents us from
+    # refactoring this and putting it in the main module, or even in a separate
+    # module then including it.
+    #
+    # Reason: the line in this file that does:
+    #
+    #   require 'arel_extensions/visitors/mssql'
+    #
+    # The error could be seen by:
+    #
+    #   1. placing the visit_ inside the visitor, or placing it in a module
+    #      then including it here.
+    #   2. replacing the `rescue nil` from aliasing trick, and printing the
+    #      error.
+    #
+    # It complains that the visit_ does not exist in the module, as if it's
+    # evaluating the module eagerly, instead of lazily like in other versions
+    # of ruby.
+    #
+    # It might be something different, but this is the first thing we should
+    # investigate.
+
+    alias_method(:old_visit_Arel_Nodes_SelectStatement, :visit_Arel_Nodes_SelectStatement) rescue nil
+    def visit_Arel_Nodes_SelectStatement o, collector
+      if !collector.value.blank? && o.limit.blank? && o.offset.blank?
+        o = o.dup
+        o.orders = []
       end
-
-      alias_method(:old_visit_Arel_Nodes_As, :visit_Arel_Nodes_As) rescue nil
-      def visit_Arel_Nodes_As o, collector
-        if o.left.is_a?(Arel::Nodes::Binary)
-          collector << '('
-          collector = visit o.left, collector
-          collector << ')'
-        else
-          collector = visit o.left, collector
-        end
-        collector << ' AS '
-        if /\A\[.*\]\z/ !~ o.right
-          collector << '['
-          collector = visit o.right, collector
-          collector << ']'
-        else
-          collector = visit o.right, collector
-        end
-        collector
-      end
-
-      alias_method(:old_primary_Key_From_Table, :primary_Key_From_Table) rescue nil
-      def primary_Key_From_Table t
-        return unless t
-
-        column_name = @connection.schema_cache.primary_keys(t.name) ||
-                      @connection.schema_cache.columns_hash(t.name).first.try(:second).try(:name)
-        column_name ? t[column_name] : nil
-      end
+      old_visit_Arel_Nodes_SelectStatement(o, collector)
     end
   end
 end
