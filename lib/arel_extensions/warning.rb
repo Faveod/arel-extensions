28bc841b21c17ea5bfb591075475c287fcea8357
Firas al-Khalil <firasalkhalil@gmail.com>
2024-12-18 12:00:07 +0000

    warning: do not emit deprecations from misuse in arel_extensions

--- lib/arel_extensions/warning.rb
+++ lib/arel_extensions/warning.rb
@@ -1,9 +1,12 @@
 module ArelExtensions
   module Warning
     def deprecated msg
-      what = caller_locations(1, 1).first.label
       kaller = caller(2..2).first
+      return if kaller.include?('lib/arel_extensions') && ENV['AREL_EXTENSIONS_IN_TEST'] != '1'
+
+      what = caller_locations(1, 1).first.label
       msg = "#{kaller}: arel_extensions: `#{what}` is now deprecated. #{msg}"
+
       if RUBY_VERSION.to_f >= 3.0
         warn msg, category: :deprecated
       else
