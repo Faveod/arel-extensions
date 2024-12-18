0c4051679640c4542a64bf9d81e3806e60c9119d
Firas al-Khalil <firas.alkhalil@faveod.com>
2024-12-18 13:27:17 +0100

    group_concat: use deprecated from the Warning module

--- lib/arel_extensions/warning.rb
+++ lib/arel_extensions/warning.rb
@@ -1,10 +1,10 @@
 module ArelExtensions
   module Warning
-    def deprecated msg
+    def deprecated msg, what: nil
       kaller = caller(2..2).first
       return if kaller.include?('lib/arel_extensions') && ENV['AREL_EXTENSIONS_IN_TEST'] != '1'
 
-      what = caller_locations(1, 1).first.label
+      what = caller_locations(1, 1).first.label if what.nil?
       msg = "#{kaller}: arel_extensions: `#{what}` is now deprecated. #{msg}"
 
       if RUBY_VERSION.to_f >= 3.0
