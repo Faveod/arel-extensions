f00bba9ea01d57a84bc1d4d0418b002a07f70b25
Firas al-Khalil <firas.alkhalil@faveod.com>
2025-01-03 16:03:04 +0100

    warning: do not propagate if caller is arel too

--- lib/arel_extensions/warning.rb
+++ lib/arel_extensions/warning.rb
@@ -33,7 +33,7 @@ module ArelExtensions
   module Warning
     def deprecated msg, what: nil
       kaller = caller(2..2).first
-      return if kaller.include?('lib/arel_extensions') && ENV['AREL_EXTENSIONS_IN_TEST'] != '1'
+      return if /lib\/(:?arel(?:_extensions)?|active_record)\// =~ kaller && ENV['AREL_EXTENSIONS_IN_TEST'] != '1'
 
       what = caller_locations(1, 1).first.label if what.nil?
       ArelExtensions.deprecator.warn "#{kaller}: `#{what}` is now deprecated. #{msg}"
